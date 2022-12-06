8ece135156767153da20aa57af617a41bf0e49c7
samsadullaev <ssamandar119@gmail.com>
2022-12-06 18:24:52 -0500

    new update to run queries

--- src/test/java/com/LibraryCt/step_def/genreBooksBorrowedMost.java
+++ src/test/java/com/LibraryCt/step_def/genreBooksBorrowedMost.java
@@ -8,32 +8,81 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.support.PageFactory;
+
+import java.sql.*;
+import java.util.List;
 
 public class genreBooksBorrowedMost {
     LoginPage loginPage=new LoginPage();
     DashBoardPage dashBoardPage=new DashBoardPage();
     BrowserUtils db_util = new BrowserUtils();
+
+    Statement statement;
+    Connection conn;
+
+    ResultSet rs;
+
+    ResultSetMetaData rsmd;
     @Given("Establish the database connection")
-    public void establish_the_database_connection() {
+    public void establish_the_database_connection() throws SQLException{
 
         loginPage.login("librarian");
 
+
+
+//        conn = DriverManager.getConnection("jdbc:mysql://34.230.35.214:3306/library2", "library2_client", "6s2LQQTjBcGFfDhY");
+//
+//        //helps us execute queries
+//         statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//
+//        //resultset will store data after execution. stores only data
+
+
+
     }
     @When("I execute query to find most popular book genre")
-    public void i_execute_query_to_find_most_popular_book_genre() {
+    public void i_execute_query_to_find_most_popular_book_genre() throws SQLException {
 
-        DB_Util.runQuery("select name from book_categories\n" +
-                "where name like 'Action and Adventure'");
+      DB_Util.runQuery("select bc.name,count(*) from book_borrow bb\n" +
+              "    inner  join books b on bb.book_id = b.id\n" +
+              "    inner join book_categories bc on b.book_category_id=bc.id\n" +
+              "group by name\n" +
+              "order by 2 desc");
+//
+//         rs = statement.executeQuery("select bc.name,count(*) from book_borrow bb\n" +
+//                 "    inner  join books b on bb.book_id = b.id\n" +
+//                 "    inner join book_categories bc on b.book_category_id=bc.id\n" +
+//                 "group by name\n" +
+//                 "order by 2 desc");
+//         rsmd = rs.getMetaData();
+
 
     }
     @Then("verify {string} is the most popular book genre.")
-    public void verify_is_the_most_popular_book_genre(String string) {
+    public void verify_is_the_most_popular_book_genre(String string) throws SQLException {
+
+        String  actualGenre = DB_Util.getFirstRowFirstColumn();
+        System.out.println(actualGenre);
+
+        Assertions.assertEquals(actualGenre,string);
+
+
+//        while (rs.next()) {
+//
+//           for(int i = 1; i<=rsmd.getColumnCount();i++){
+//
+//               String actual = rsmd.getColumnName(i) + rs.getString(i);
+//
+//               if(actual.equals(string)){
+//                   Assertions.assertEquals(string,actual);
+//               }
+//
+//           }
 
 
 
-        String actual = DB_Util.getCellValue(0,0);
-
-        Assertions.assertEquals(string,actual);
+        }
     }
 
-}
+
